b9ae2f9204d2a0a87cd462c2dad9d94b4d86fd63
Dawid <35502248+byczkos@users.noreply.github.com>
2018-02-02 14:45:18 +0100

    Jjddjav 1.2 (#5)
    *  oldproject
    
    *  Add menu class

---
+++ src/view/Menu.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public void showOption() {
+        System.out.println("1. Znajdź najbliższą stację ładowania");
+        System.out.println("2. Znajdź stacje ładowania w podanym promieniu");
+        System.out.println("3. Znajdź stacje ładowania w danym państwie lub mieście ");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.print("Wybierz opcje: ");
+                int x = Integer.valueOf(scanner.nextLine());
+                if (x == 1 || x == 2 || x == 3) {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+            }
+
+            System.out.println("Błędna wartość, podaj liczby z zakresu [1,2,3]");
+        }
+
+    }
+
+}
