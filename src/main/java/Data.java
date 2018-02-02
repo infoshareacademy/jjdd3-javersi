555bad12362cde4cafc7e56e7a477f8ad50acb4e
Jakub Watus <kwatus@gmail.com>
2018-02-02 15:45:37 +0100

    Find closest station

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -1,20 +1,54 @@
+import model.AddressInfo;
+import model.ChargingPoint;
+import model.Country;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.StrictMath.sqrt;
+
 public class Data {
 
-    public void dataIn(){
+    public void dataIn() {
 
     }
 
-    public void findChargingStationAtTown(){
+////////////////////////// FIND STATION AT TOWN ////////////////////////////////////////////////////////////////////////
+
+    public List<ChargingPoint> findChargingStationAtTown(List<ChargingPoint> points, String town, Country country) {
+
+        List<ChargingPoint> chargingPoints = new ArrayList<>();
+
+        for (ChargingPoint p : points) {
+            if (p.getOperatorInfo().getAddressInfo().getTown() == town) {
+                chargingPoints.add(p);
+            }
+        }
+
+        return chargingPoints;
+    }
+    ////////////////////////// FIND CLOSEST STATION ////////////////////////////////////////////////////////////////////////
+
+    public void findClosestChargingStation(List<ChargingPoint> points, double longitude, double latitude) {
+
+        List<ChargingPoint> chargingPoints = new ArrayList<>();
+
+        double distance;
+
+        double closest = Double.MAX_VALUE;
+
+        for (ChargingPoint p : points) {
+
+            distance = sqrt(Math.pow(latitude - p.getOperatorInfo().getAddressInfo().getLatitude(), 2)
+                    + Math.pow(longitude - p.getOperatorInfo().getAddressInfo().getLongitude(), 2));
+
+            if (distance < closest) {
+                closest = distance;
+            }
+
+        }
 
     }
-
-    public void findClosestChargingStation(){
-
-    }
-
-    public void findChargingStationAtArea(){
-
-    }
-
 
 }
+
