aee481589c466ad5a8f6c81893412a0b41fe831c
Jakub Watus <kwatus@gmail.com>
2018-02-09 15:39:32 +0100

    Initial tests

--- src/test/java/controller/DataFilterTest.java
+++ src/test/java/controller/DataFilterTest.java
@@ -1,5 +1,30 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataFilterTest {
 
+    @Test
+    void findChargingStationAtTown() {
+
+
+
+    }
+
+    @Test
+    void findClosestChargingStation() {
+
+
+
+
+    }
+
+    @Test
+    void findChargingStationAtArea() {
+
+
+
+    }
 }
