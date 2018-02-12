969c115fc06f124913904753785f7118825193b5
Michał Matosek <michal.matosek@wp.pl>
2018-02-12 14:18:36 +0100

    Added [UnitSettings][class] and fixed methods

--- src/main/java/controller/UnitSettings.java
+++ src/main/java/controller/UnitSettings.java
@@ -1,13 +1,16 @@
 package controller;
 
 public class UnitSettings {
+    private static final double  R = 6371;//km
 
-    public static double longitudeToMeters(double longitude) {
-        return longitude * 111196.672;
+    public static double distanceBetweenTwoPoints(double latitude1, double latitude2, double longitude1, double longitude2) {
+
+        double distanceLatitude = Math.toRadians(latitude2 - latitude1);
+        double distanceLongitude = Math.toRadians(longitude2 - longitude1);
+        double a = Math.sin(distanceLatitude / 2) * Math.sin(distanceLatitude / 2) +
+                Math.cos(Math.toRadians(longitude1)) * Math.cos(Math.toRadians(longitude2)) *
+                        Math.sin(distanceLongitude / 2) * Math.sin(distanceLongitude / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c;
     }
-
-    public static double latitudeToMeters(double latitude) {
-        return latitude * 111196.672 * Math.cos(latitude * Math.PI / 180);
-    }
-
 }
