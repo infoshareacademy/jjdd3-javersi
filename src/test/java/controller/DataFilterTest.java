aeb72c49e18c559029cce48b379accb7d4081b9d
Jakub Watus <kwatus@gmail.com>
2018-02-13 16:27:17 +0100

    FindChargingPointAtArea unit test

--- src/test/java/controller/DataFilterTest.java
+++ src/test/java/controller/DataFilterTest.java
@@ -11,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DataFilterTest {
 
-
     @Test
     void shouldProperlySetFirstAndLastName() {
 
@@ -39,12 +38,51 @@ class DataFilterTest {
 
         List<ChargingPoint> chargingPointList = new ArrayList<>();
 
-
     }
 
     @Test
     void findChargingStationAtArea() {
 
+        List<ChargingPoint> chargingPointList = new ArrayList<>();
+
+        ChargingPoint pointOne = new ChargingPoint();
+        AddressInfo addressInfoOne = new AddressInfo();
+        addressInfoOne.setLatitude(15);
+        addressInfoOne.setLongitude(30);
+        addressInfoOne.setId(1111);
+        pointOne.setAddressInfo(addressInfoOne);
+        chargingPointList.add(pointOne);
+
+        ChargingPoint pointTwo = new ChargingPoint();
+        AddressInfo addressInfoTwo = new AddressInfo();
+        addressInfoTwo.setLatitude(87);
+        addressInfoTwo.setLongitude(82);
+        addressInfoTwo.setId(2222);
+        pointTwo.setAddressInfo(addressInfoTwo);
+        chargingPointList.add(pointTwo);
+
+        ChargingPoint pointThree = new ChargingPoint();
+        AddressInfo addressInfoThree = new AddressInfo();
+        addressInfoThree.setLatitude(16);
+        addressInfoThree.setLongitude(30);
+        addressInfoThree.setId(3333);
+        pointThree.setAddressInfo(addressInfoThree);
+        chargingPointList.add(pointThree);
+
+        ChargingPoint pointFour = new ChargingPoint();
+        AddressInfo addressInfoFour = new AddressInfo();
+        addressInfoFour.setLatitude(88);
+        addressInfoFour.setLongitude(88);
+        addressInfoFour.setId(4444);
+        pointFour.setAddressInfo(addressInfoFour);
+        chargingPointList.add(pointFour);
+
+        List<ChargingPoint> returnedList = DataFilter.findChargingStationAtArea(chargingPointList,
+                30, 15, 200);
+
+        assertEquals(2, returnedList.size());
+        assertEquals(1111, returnedList.get(0).getAddressInfo().getId());
+        assertEquals(3333, returnedList.get(1).getAddressInfo().getId());
 
     }
 }
