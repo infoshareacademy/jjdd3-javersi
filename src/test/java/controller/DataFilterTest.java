34bf1926e8d456eaf44ddd1cabb0c501a7d66715
Jakub Watus <kwatus@gmail.com>
2018-02-13 14:45:59 +0100

    FindChargingPointInTown unit test

--- src/test/java/controller/DataFilterTest.java
+++ src/test/java/controller/DataFilterTest.java
@@ -1,32 +1,40 @@
 package controller;
 
+import model.AddressInfo;
 import model.ChargingPoint;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataFilterTest {
 
 
     @Test
-    void findChargingStationAtTown() {
+    void shouldProperlySetFirstAndLastName() {
+        List<ChargingPoint> chargingPointList = new ArrayList<>();
+        ChargingPoint point = new ChargingPoint();
+        AddressInfo addressInfo = new AddressInfo();
+        addressInfo.setTown("Warsaw");
+        point.setAddressInfo(addressInfo);
+        chargingPointList.add(point);
 
-
-
-
+        assertAll("Get town",
+                () -> assertEquals("Warsaw", point.getAddressInfo().getTown(), "Town:")
+        );
     }
+
 
     @Test
     void findClosestChargingStation() {
-
-
-
 
 
     }
 
     @Test
     void findChargingStationAtArea() {
-
 
 
     }
