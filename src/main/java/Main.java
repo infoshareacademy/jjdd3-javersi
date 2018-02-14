1b6e65de8e2c6cc1b7f4a57d2a12d6fc2561d67d
Michał Matosek <michal.matosek@wp.pl>
2018-02-14 16:54:43 +0100

    Added exit option and fixed previous changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import controller.JsonLoader;
 import controller.JsonParser;
 import model.ChargingPoint;
 import view.Menu;
+import view.PointDisplayer;
 import view.Settings;
 
 import java.io.IOException;
@@ -21,25 +22,38 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        Settings.run();
         Menu menu = new Menu();
-        switch (menu.pickOption()) {
-                ChargingPoint chargingPoint = DataFilter.findClosestChargingStation(chargingPointList, -110, 40);
-                PointDisplayer.showChargingPointProperties(chargingPoint);
-                break;
-            }
-            case 2: {
-                List<ChargingPoint> chargingPointListAtArea = DataFilter.findChargingStationAtArea(chargingPointList, -99.36, 39.16, 500);
-                PointDisplayer.showAllAvailablePointsProperties(chargingPointListAtArea);
-                break;
-            }
-            case 3: {
-                List<ChargingPoint> chargingPointListAtTown = DataFilter.findChargingStationAtTown(chargingPointList, "Whistler");
-                PointDisplayer.showAllAvailablePointsProperties(chargingPointListAtTown);
-                break;
+
+        outerloop:
+        while (true) {
+
+            switch (menu.pickOption()) {
+                case 1: {
+                    ChargingPoint chargingPoint = DataFilter.findClosestChargingStation(chargingPointList, -110, 40);
+                    PointDisplayer.showChargingPointProperties(chargingPoint);
+                    break;
+                }
+                case 2: {
+                    List<ChargingPoint> chargingPointListAtArea = DataFilter.findChargingStationAtArea(chargingPointList, -99.36, 39.16, 500);
+                    PointDisplayer.showAllAvailablePointsProperties(chargingPointListAtArea);
+                    break;
+                }
+                case 3: {
+                    List<ChargingPoint> chargingPointListAtTown = DataFilter.findChargingStationAtTown(chargingPointList, "Whistler");
+                    PointDisplayer.showAllAvailablePointsProperties(chargingPointListAtTown);
+                    break;
+                }
+                case 4: {
+                    Settings.show();
+                    break;
+                }
+
+                case 5: {
+                    break outerloop;
+                }
             }
         }
 
     }
+
 }
--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -9,6 +9,8 @@ public class Menu {
         System.out.println("1. Znajdź najbliższą stację ładowania");
         System.out.println("2. Znajdź stacje ładowania w podanym promieniu");
         System.out.println("3. Znajdź stacje ładowania w danym państwie lub mieście ");
+        System.out.println("4. Ustawienia");
+        System.out.println("5. Wyjście");
 
         Scanner scanner = new Scanner(System.in);
 
@@ -16,11 +18,12 @@ public class Menu {
             try {
                 System.out.print("Wybierz opcje: ");
                 int x = Integer.valueOf(scanner.nextLine());
-                if (x == 1 || x == 2 || x == 3) {
+                if (x == 1 || x == 2 || x == 3 || x == 4 || x == 5) {
                     return x;
                 }
             } catch (NumberFormatException e) {
             }
         }
     }
+
 }
