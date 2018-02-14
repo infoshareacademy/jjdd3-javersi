2f76eec94180f0a93f1ba11aa8aec5d0a43a06e3
Jakub Watus <kwatus@gmail.com>
2018-02-14 14:58:53 +0100

    DataFiltertest findClosestStation unit test

--- src/test/java/controller/DataFilterTest.java
+++ src/test/java/controller/DataFilterTest.java
@@ -38,6 +38,35 @@ class DataFilterTest {
 
         List<ChargingPoint> chargingPointList = new ArrayList<>();
 
+        ChargingPoint pointOne = new ChargingPoint();
+        AddressInfo addressInfoOne = new AddressInfo();
+        addressInfoOne.setLatitude(15);
+        addressInfoOne.setLongitude(30);
+        addressInfoOne.setId(1111);
+        pointOne.setAddressInfo(addressInfoOne);
+        chargingPointList.add(pointOne);
+
+        ChargingPoint pointTwo = new ChargingPoint();
+        AddressInfo addressInfoTwo = new AddressInfo();
+        addressInfoTwo.setLatitude(87);
+        addressInfoTwo.setLongitude(82);
+        addressInfoTwo.setId(2222);
+        pointTwo.setAddressInfo(addressInfoTwo);
+        chargingPointList.add(pointTwo);
+
+        ChargingPoint pointThree = new ChargingPoint();
+        AddressInfo addressInfoThree = new AddressInfo();
+        addressInfoThree.setLatitude(18);
+        addressInfoThree.setLongitude(47);
+        addressInfoThree.setId(3333);
+        pointThree.setAddressInfo(addressInfoThree);
+        chargingPointList.add(pointThree);
+
+        ChargingPoint returnedPoint = DataFilter.findClosestChargingStation(chargingPointList,
+                31, 16);
+
+        assertEquals(1111, returnedPoint.getAddressInfo().getId());
+
     }
 
     @Test
