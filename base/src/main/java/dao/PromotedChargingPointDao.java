f2be0e0b8f991fcc72663eb1f1f5f31d203d284d
Karol Jackiewicz <kwjackiewicz@gmail.com>
2018-03-08 15:24:14 +0100

    PromotetChargingPoint added

---
+++ base/src/main/java/dao/PromotedChargingPointDao.java
@@ -0,0 +1,39 @@
+package dao;
+
+import model.ChargingPoint;
+import model.PromotedChargingPoint;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+@Stateless
+public class PromotedChargingPointDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public int save(PromotedChargingPoint pcp) {
+        entityManager.persist(pcp);
+        return pcp.getChargingPointId();
+    }
+
+    public void delete(int chargingPointId) {
+        final PromotedChargingPoint pcp = entityManager.find(PromotedChargingPoint.class, chargingPointId);
+        if(pcp != null) {
+            entityManager.remove(pcp);
+        }
+    }
+    
+    public PromotedChargingPoint findById(Long id) {
+        return entityManager.find(PromotedChargingPoint.class, id);
+    }
+
+    public List<PromotedChargingPoint> findAll() {
+        final Query query = entityManager.createQuery("SELECT pcp FROM PromotedChargingPoint pcp");
+        return query.getResultList();
+    }
+}
+
