960334fb60d7b1fdeadf12fa20fc6e9662eca353
Dawid Byczkowski <dbyczkowski@gmail.com>
2018-03-21 17:04:39 +0100

    Added UsersDao and UserNames entity

---
+++ base/src/main/java/model/UserNames.java
@@ -0,0 +1,64 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USER_INFO")
+public class UserNames {
+
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "locale")
+    private String locale;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
