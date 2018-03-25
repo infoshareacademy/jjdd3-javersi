8f52e27a53c8d5cea0edef6a1405055c2506a9dc
Dawid Byczkowski <35502248+byczkos@users.noreply.github.com>
2018-03-25 19:20:37 +0200

    Mergefix (#76)
    * added search in town statistics
    
    * added uploading data from api
    
    * added country statistics
    
    * Added wrong input security to every servlets
    
    * added logs to Uploading from api
    
    * added logs to Uploading from api 2
    
    * changed web.xml
    
    * fixed docker
    
    * Fix bugs

--- web/src/main/java/servlets/FindTheClosestServlet.java
+++ web/src/main/java/servlets/FindTheClosestServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import cdi.AppPropertiesBean;
 import cdi.ChargingPointToDtoConverterBean;
+import commons.Formaters;
 import controller.CoordinatesConverter;
 import controller.DataFilter;
 import dao.ChargingPointDao;
@@ -64,29 +65,64 @@ public class FindTheClosestServlet extends HttpServlet {
         String directionLong = req.getParameter("directionLong");
         String degreesLong = req.getParameter("degreesLong");
         String minutesLong = req.getParameter("minutesLong");
-        String secondLong = req.getParameter("secondLong");
+        String secondsLong = req.getParameter("secondLong");
         String directionLati = req.getParameter("directionLati");
         String degreesLati = req.getParameter("degreesLati");
         String minutesLati = req.getParameter("minutesLati");
-        String secondLati = req.getParameter("secondLati");
+        String secondsLati = req.getParameter("secondLati");
 
-        if (degreesLong == null || degreesLong.isEmpty() || minutesLong == null || minutesLong.isEmpty()) {
+        boolean isDegreesLongNull = (degreesLong == null || degreesLong.isEmpty());
+        boolean isMinutesLongNull = (minutesLong == null || minutesLong.isEmpty());
+        boolean isSecondsLongNull = (secondsLong == null || secondsLong.isEmpty());
+        boolean isDegreesLatiNull = (degreesLati == null || degreesLati.isEmpty());
+        boolean isMinutesLatiNull = (minutesLati == null || minutesLati.isEmpty());
+        boolean isSecondsLatiNull = (secondsLati == null || secondsLati.isEmpty());
+
+        if ((isDegreesLongNull && isMinutesLongNull && isSecondsLongNull)
+                && (isDegreesLatiNull && isMinutesLatiNull && isSecondsLatiNull)) {
             dataModel.put("body_template", "find-the-closest");
-
+            dataModel.put("current_unit", Formaters.naturalFormat(appPropertiesBean.getCurrentUnit().name()));
+            dataModel.put("title", "Find the closest charging point");
+        } else if ((isDegreesLongNull && isMinutesLongNull && isSecondsLongNull)
+                || (isDegreesLatiNull && isMinutesLatiNull && isSecondsLatiNull)) {
+            errorMessages(dataModel);
         } else {
-            double longitude = coordinatesConverter.convertCoordinatesToDecimal(directionLong, degreesLong, minutesLong, secondLong);
-            double latitude = coordinatesConverter.convertCoordinatesToDecimal(directionLati, degreesLati, minutesLati, secondLati);
-            List<ChargingPoint> chargingPointsList = new ArrayList<>();
-            ChargingPoint chargingPoint = dataFilter
-                    .findClosestChargingStation(chargingPointDao.findAll(), longitude,
-                            latitude);
-            chargingPointsList.add(chargingPoint);
-            List<ChargingPointDto> chargingPointsDtoList = chargingPointToDtoConverterBean.convertList(chargingPointsList);
-            dataModel.put("body_template", "results");
-            dataModel.put("chargingPoints", chargingPointsDtoList);
-            dataModel.put("latitude", latitude);
-            dataModel.put("longitude", longitude);
-            dataModel.put("google_api_key", appPropertiesBean.getGoogleApiKey());
+            if (isDegreesLongNull) degreesLong = "0";
+            if (isMinutesLongNull) minutesLong = "0";
+            if (isSecondsLongNull) secondsLong = "0";
+            if (isDegreesLatiNull) degreesLati = "0";
+            if (isMinutesLatiNull) minutesLati = "0";
+            if (isSecondsLatiNull) secondsLati = "0";
+
+            if (isStringInRange(degreesLati, 0, 90)
+                    && isStringInRange(minutesLati, 0, 60)
+                    && isStringInRange(secondsLati, 0, 60)
+                    && isStringInRange(degreesLong, 0, 180)
+                    && isStringInRange(minutesLong, 0, 60)
+                    && isStringInRange(secondsLong, 0, 60)) {
+                try {
+                    double longitude = coordinatesConverter.convertCoordinatesToDecimal(directionLong, degreesLong, minutesLong, secondsLong);
+                    double latitude = coordinatesConverter.convertCoordinatesToDecimal(directionLati, degreesLati, minutesLati, secondsLati);
+                    List<ChargingPoint> chargingPointsList = new ArrayList<>();
+                    ChargingPoint chargingPoint = dataFilter
+                            .findClosestChargingStation(chargingPointDao.findAll(), longitude,
+                                    latitude);
+                    chargingPointsList.add(chargingPoint);
+                    List<ChargingPointDto> chargingPointsDtoList = chargingPointToDtoConverterBean.convertList(chargingPointsList);
+                    dataModel.put("body_template", "results");
+                    dataModel.put("chargingPoints", chargingPointsDtoList);
+                    dataModel.put("latitude", latitude);
+                    dataModel.put("longitude", longitude);
+                    dataModel.put("google_api_key", appPropertiesBean.getGoogleApiKey());
+                } catch (Exception e) {
+                    dataModel.put("body_template", "find-the-closest");
+                    dataModel.put("title", "Find the closest charging point");
+                    dataModel.put("error", "No charging points were found");
+                    LOG.error("No charging points were found");
+                }
+            } else {
+                errorMessages(dataModel);
+            }
         }
         resp.setContentType("text/html;charset=UTF-8");
 
